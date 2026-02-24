fdf836ed741a9435adec98cd63842c702281e4a5
DEV SINGH <devsingh120307@gmail.com>
2026-02-24 22:06:49 +0530

    class2.2

---
+++ class2.2.js
@@ -0,0 +1,28 @@
+var count;
+document.write("starting Loop"+"<br/>");
+for(count = 0; count < 10; count++){
+    document.write("CurrentCount:"+count);
+    document.write("<br/>");
+}
+document.write("Loop Stopped!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
